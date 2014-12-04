73ecb41830597dde5cae96977b6605a53a933d04
Shan Hasan <shan119@hotmail.co.uk>
2014-12-04 17:09:29 +0000

    updated index, css and controller to reflect additional search results

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -1,6 +1,7 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource){
 
-  var searchResource = $resource('https://api.github.com/search/users');
+  var searchResource = $resource('https://api.github.com/users');
+  var searchRepo = $resource('');
 
   $scope.doSearch = function() {
     if($scope.searchResult !== "") {
@@ -15,3 +16,24 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
     
   };
 });
+
+
+ // function GitHubCtrl($scope, $http) {
+ //      $scope.getGitInfo = function () {
+ //         $scope.userNotFound = false;
+ //         $scope.loaded = false;
+ //         $http.get("https://api.github.com/users/" + $scope.username)
+ //               .success(function (data) {
+ //                  if (data.name == "") data.name = data.login;
+ //                  $scope.user = data;
+ //                  $scope.loaded = true;
+ //               })
+ //               .error(function () {
+ //                  $scope.userNotFound = true;
+ //               });
+ //         $http.get("https://api.github.com/users/" + $scope.username + "/repos").success(function (data) {
+ //            $scope.repos = data;
+ //            $scope.reposFound = data.length > 0;
+ //         });
+ //      }
+ //   }
