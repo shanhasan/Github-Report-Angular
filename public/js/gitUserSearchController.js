781f43fd8f90de92b7cd479d1ec724cd107e65ed
ablease <alexblease@gmail.com>
2014-12-04 14:12:59 +0000

    reconfiguring bower for heroku deployment

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -3,10 +3,13 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
   var searchResource = $resource('https://api.github.com/search/users');
 
   $scope.doSearch = function() {
-    $scope.$apply();
-    $scope.searchResult = searchResource.get({
-      q: $scope.searchTerm
+    if($scope.searchResult !== "") {
+      $scope.$apply();
+      $scope.searchResult = searchResource.get({
+        q: $scope.searchTerm,
+        access_token: "91d8e96fd50ab4006e5925eb6471c6784894b8d8" 
     });
+  };
     // $scope.image.width = img.width;
     // $scope.image.height = img.height;
     
