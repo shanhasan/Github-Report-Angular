5d9d23b18a3a6b33588211a50567dbc175018c18
ablease <alexblease@gmail.com>
2014-12-04 20:42:13 +0000

    User list item results now display total number of repos! NEEDS REFACTOR!!

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -1,21 +1,55 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource){
 
   var searchResource = $resource('https://api.github.com/search/users');
+  var count = 0, result_length, search_result = [];
 
   $scope.doSearch = function() {
-    if($scope.searchResult !== "") {
-      $scope.$apply();
-      $scope.searchResult = searchResource.get({
+
+    $scope.hello = 'uefsd';
+    // if($scope.searchResult !== "") {
+      searchResource.get({
         q: $scope.searchTerm,
         access_token: "91d8e96fd50ab4006e5925eb6471c6784894b8d8" 
+      }, function(results){
+
+                      $scope.hello = "yoooo"
+
+
+          result_length = results.items.length;
+
+          results.items.forEach(function(item){
+          var userRepos = $resource(item.repos_url);
+          userRepos.query({
+            access_token: "91d8e96fd50ab4006e5925eb6471c6784894b8d8"
+          }, function(repos) {
+                          $scope.hello = "yoooo"
+
+            var repo_count = repos.length;
+            item.repoCount = repo_count;
+            search_result.push(item)
+
+            count++;
+
+            if (count === result_length) {
+              // $rootScope.$$phase || $rootScope.$apply();
+              // $scope.$apply(function(){
+                $scope.searchResult = search_result;
+              // });
+              // console.log('found all')
+              // console.log($scope.searchResult, search_result)
+              $scope.hello = "yoooo"
+            }
+            // console.log($scope.searchResult);
+
+          });
+        });  
       });
-    };
-    // if($scope.searchRepos !== ""){
-    //   $scope.apply();
-      // $scope.searchRepos = searchResource.get({
-      //   q: $scope.searchTerm + '/repos',
-      //   access_token: "91d8e96fd50ab4006e5925eb6471c6784894b8d8"
-      // });
-    // };
-  };
+    // }
+  } 
 });
+
+
+
+          // Do query
+          // When we get the result back from the query, do a callback, and add information to our user
+          // then add the user to our results array
