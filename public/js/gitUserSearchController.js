d2e6dde49ce3cf16ec652190bf0978e2d80df496
Shan Hasan <shan119@hotmail.co.uk>
2014-12-05 12:05:57 +0000

    Refactored search controller

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -7,23 +7,23 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
       searchResource.get({
         q: $scope.searchTerm,
         access_token: "91d8e96fd50ab4006e5925eb6471c6784894b8d8" 
-      }, function(results){
-        var result_length = results.items.length;
+        }, function(results){
+          var result_length = results.items.length;
 
-        results.items.forEach(function(item){
-        var userRepos = $resource(item.repos_url);
-        userRepos.query({
-          access_token: "91d8e96fd50ab4006e5925eb6471c6784894b8d8"
-        }, function(repos) {
-          var repo_count = repos.length;
-          item.repoCount = repo_count;
-          search_result.push(item)
-          count++;
-          if (count === result_length) {
-              $scope.searchResult = search_result;
+          results.items.forEach(function(item){
+            var userRepos = $resource(item.repos_url);
+            userRepos.query({
+            access_token: "91d8e96fd50ab4006e5925eb6471c6784894b8d8"
+            }, function(repos) {
+              var repo_count = repos.length;
+              item.repoCount = repo_count;
+              search_result.push(item)
+              count++;
+              if (count === result_length) {
+                $scope.searchResult = search_result;
           }
         });
       });  
     });
-  } 
+  }  
 });
